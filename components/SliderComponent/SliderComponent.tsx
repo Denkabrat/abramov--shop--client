15cde191335403d542b12991d915c45ff518fdb1
Denkabrat <denis46778@gmail.com>
2023-12-08 17:27:52 +0300

    client

--- components/SliderComponent/SliderComponent.tsx
+++ components/SliderComponent/SliderComponent.tsx
@@ -1,6 +1,5 @@
-//Global
 import Image from "next/image";
-import { Navigation, Pagination, EffectCreative,EffectCube } from "swiper/modules";
+import { Navigation, Pagination, EffectCreative, EffectCube } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 //Components
@@ -13,41 +12,52 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/effect-creative";
 
-const SliderComponent = ({img}:{img:[]}) => {
+// Кастомный loader для изменения параметров запроса изображения
+const customLoader = ({ src, width, quality }:any) => {
+  return `${src}?w=${width}&q=${quality || 75}`;
+};
 
-    return (
-        <Swiper
-            effect={'effect-creative'}
-            creativeEffect={{
-                prev: {
-                    translate: [0, 0, -400],
-                },
-                next: {
-                    translate: ["100%", 0, 0],
-                },
-                }}
-            modules={[Navigation, Pagination, EffectCreative]}
-            spaceBetween={50}
-            slidesPerView={1}
-            navigation
-            pagination={{ clickable: true }}
-            loop={true}
-            >
-                   
-          
-
-        {
-            img.map((imgUrl: string,id:number) => (
-                 <SwiperSlide  key={id}>
-                        {
-                            img ? <div className="main-wrapper-photo"><div className="one-photo"><Image src={`${process.env.NEXT_PUBLIC_API_URL}/` + imgUrl} width={500} height={500} className="photo" alt="black" /></div></div> : <Icons id="spiner"/>
-                        }
-                  
-                 </SwiperSlide>
-            ))
-        }
-          </Swiper>
-    );
+const SliderComponent = ({ img }: { img: string[] }) => {
+  return (
+    <Swiper
+      effect={'effect-creative'}
+      creativeEffect={{
+        prev: {
+          translate: [0, 0, -400],
+        },
+        next: {
+          translate: ["100%", 0, 0],
+        },
+      }}
+      modules={[Navigation, Pagination, EffectCreative]}
+      spaceBetween={50}
+      slidesPerView={1}
+      navigation
+      pagination={{ clickable: true }}
+      loop={true}
+    >
+      {img.map((imgUrl: string, id: number) => (
+        <SwiperSlide key={id}>
+          {img ? (
+            <div className="main-wrapper-photo">
+              <div className="one-photo">
+                <Image
+                  loader={customLoader} // Передаем кастомный loader
+                  src={`${process.env.NEXT_PUBLIC_API_URL}/` + imgUrl}
+                  width={500}
+                  height={500}
+                  className="photo"
+                  alt="black"
+                />
+              </div>
+            </div>
+          ) : (
+            <Icons id="spiner" />
+          )}
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
 };
 
 export default SliderComponent;
