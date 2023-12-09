02b9b88f31ebe234779877d8ce4db45236b8af63
Denkabrat <denis46778@gmail.com>
2023-12-09 17:55:52 +0300

    client

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     swcMinify:true,
     optimizeFonts:true,
     images:{
-        domains: ['abramov-shop-server.onrender.com'],
+        domains: ['abramov-shop-server.vercel.app/'],
         minimumCacheTTL:1500000
     }
 };
